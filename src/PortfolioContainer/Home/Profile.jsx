419542c32a5d7c96eb0bc958ff2804c80fb8e852
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2022-10-24 13:29:50 +0000

    add profile page

---
+++ src/PortfolioContainer/Home/Profile.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import Typewriter from 'typewriter-effect'
+import './Profile.css'
+
+const Profile = () => {
+  return ( 
+    <div className='profile-container'>
+        <div className="profile-parent">
+            <div className="profile-detail">
+                <div className="colz">
+                    <div className="colz-icons">
+                        <a href="#">
+                            <i className='fa fa-facebook-square'></i>
+                        </a>
+                        <a href="#">
+                            <i className='fa fa-google-plus-square'></i>
+                        </a>
+                        <a href="#">
+                            <i className='fa fa-instagram'></i>
+                        </a>
+                        <a href="#">
+                            <i className='fa fa-youtube-square'></i>
+                        </a>
+                        <a href="#">
+                            <i className='fa fa-twitter'></i>
+                        </a>
+                    </div>
+                </div>
+           
+
+                <div className="profile-details-name">
+                    <span className="primary-text">
+                        { '' }
+                        Hello, I'M <span className="highlighted-text">Shafiu</span>
+                    </span>
+                </div>
+
+                <div className="profile-details-role">
+                    <span className="primary-text">
+                        { '' }
+                        <h1>
+                            <Typewriter
+                                options={{
+                                    strings: [
+                                    'Enthuasiastic Dev ',
+                                    'Full Stak Dev',
+                                    'ML Engineer',
+                                    'React Native Dev',
+                                    'Laravel Dev'
+                                    ],
+                                    autoStart: true,
+                                    loop: true,
+                                }}
+                            />
+                        </h1>
+                        <span className="profile-role-tagline">
+                            Knack of building applications with front  and back
+                        </span>
+                    </span>
+                </div>
+
+                <div className="profile-options">
+                    <button className='btn primary-btn'>
+                        { '' }
+                        Hire Me { '' }
+                    </button>
+                    <a href="shafiu.pdf" download='Shafiu Ibn Yushawu.pdf'>
+                        <button className='btn highlighted-btn'>Get Resume</button>
+                    </a>
+                </div>                 
+                
+            </div>
+            <div className="profile-picture">
+                <div className="profile-picture-background"> </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Profile
