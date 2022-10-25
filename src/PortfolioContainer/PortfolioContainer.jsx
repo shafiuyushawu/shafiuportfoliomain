f64a8f7d894160b18d524bc6b193336ba35842cf
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2022-10-25 17:03:53 +0000

    added scrollservice

--- src/PortfolioContainer/PortfolioContainer.jsx
+++ src/PortfolioContainer/PortfolioContainer.jsx
@@ -6,7 +6,7 @@ function PortfolioContainer() {
     const mapAllScreens = () => {
         return(
             TOTAL_SCREENS.map(( screen ) => (
-                (screen.component) ? <screen.component screenName={ screen.screen_name } id={ screen.screen_name } /> : <div key={ screen.screen_name }></div>
+                screen.component ? <screen.component screenName={ screen.screen_name } id={ screen.screen_name } /> : <div key={ screen.screen_name }></div>
             ))
         )
     }
