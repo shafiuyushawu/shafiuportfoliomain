d86a99a4692bcafd88bc983b330e70fba0e97be8
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-08-08 16:55:18 +0000

    Installed daisyui

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+// const daisyui = require('daisyui');
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
