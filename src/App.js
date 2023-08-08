82af4515f90c2bcb6ca713a6ffa636c929c94fbc
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-08-08 16:56:36 +0000

    Adjusted items in portfolion

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './PortfolioContainer/Home/Home';
-import AboutMe from './PortfolioContainer/AboutMe/AboutMe';
-import Resume from './PortfolioContainer/Resume/Resume';
-import Projects from './PortfolioContainer/Projects/Projects';
-import Testimonial from './PortfolioContainer/Testimonial/Testimonial';
-import ContactMe from './PortfolioContainer/ContactMe/ContactMe';
+import PortfolionHome from './PortfolioContainer/PortfolionHome';
 
 const App = () => (
   <div>
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/aboutme" element={<AboutMe />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/testimonial" element={<Testimonial />} />
-        <Route path="/contactme" element={<ContactMe />} />
+        <Route path="/" element={<PortfolionHome />} />
       </Routes>
     </Router>
   </div>
---
+++ src/PortfolioContainer/PortfolionHome.jsx
@@ -0,0 +1,19 @@
+import Home from './Home/Home';
+import AboutMe from './AboutMe/AboutMe';
+import Resume from './Resume/Resume';
+import Testimonial from './Testimonial/Testimonial';
+import Projects from './Projects/Projects';
+import ContactMe from './ContactMe/ContactMe';
+
+const PortfolionHome = () => (
+  <div>
+    <Home />
+    <AboutMe />
+    <Resume />
+    <Testimonial />
+    <Projects />
+    <ContactMe />
+  </div>
+);
+
+export default PortfolionHome;
